45c85c01bf14608a2fcb4d35344b54362ac15340
Sreejeeth Ramprasad <sreejeeth.ramprasad1996@gmail.com>
2019-02-25 14:38:44 +0530

    First commit

---
+++ market/supermarket/views.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+#from sirmvit.models import Studentdbs,Category
+from django.shortcuts import render, get_object_or_404
+
+
+# Create your views here.
